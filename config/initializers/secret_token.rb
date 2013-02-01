c6af1a7f7c37e7ec613a9c7c0a5bb1207b50bd75
Parker Smith <parker@opensitter.com>
2013-01-31 22:10:04 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '591b456a29236a52017947d19e4cb69f891c54444a068d019315fbf985b19e6187b3d2571719d2724bb06d236dcd4f99fb1afe81452ed74eb024e993a10eca24'
